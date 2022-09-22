27a5067b81aa89c100a3a8c59caebb8d5c3d9d32
Jakub Klik <Jakub.klik6@gmail.com>
2022-09-22 17:48:10 +0200

    Update loginApp.cs

--- loginApp.cs
+++ loginApp.cs
@@ -1,78 +1,54 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace RegisteringAndLogginIn
 {
     class Program
     {
+
+        static string username;
+        static string password;
+
         static void Main(string[] args)
         {
-            generalException:
-            Console.WriteLine("\nHello new user!!");
-            Console.WriteLine("Press 1 to login or 2 to register you account");
+            Register();
+            Login();
+            Console.Read();
+        }
 
-            string userInput = Console.ReadLine();
-            
-            try
+        public static void Register()
+        {
+            Console.WriteLine("Please enter your username");
+            username = Console.ReadLine();
+            Console.WriteLine("Please enter your password");
+            password = Console.ReadLine();
+            Console.WriteLine("Registration completed");
+            Console.WriteLine("-------------------------------------------");
+        }
+
+        public static void Login()
+        {
+            Console.WriteLine("Please enter your username");
+            if(username == Console.ReadLine())
             {
-                int userIpnutAsNumber = int.Parse(userInput);
-
-                if (userIpnutAsNumber == 1) 
+                Console.WriteLine("Please enter your password");
+                if(password == Console.ReadLine())
                 {
-                    Console.Write("Please eneter your login: ");
-                    string userLogin = Console.ReadLine();
-                    Console.Write("Please eneter your login: ");
-                    string userPassword = Console.ReadLine();
-                    Console.WriteLine("You are not registered");
-                    goto generalException;
-                }
-
-                else if (userIpnutAsNumber == 2)
-                {
-                    Console.Write("Please eneter your login: ");
-                    string userNewLogin = Console.ReadLine();
-                    Console.Write("Please eneter your Password: ");
-                    string userNewPassword = Console.ReadLine();
-
-                    validNumber:
-                    Console.WriteLine("You have successfuly registered, now press 1 to login");
-
-                    string registeredUserInput = Console.ReadLine();
-                    int registeredUserInputAsNumber = int.Parse(registeredUserInput);
-
-                    if (int.TryParse(registeredUserInput,out registeredUserInputAsNumber))
-                    {
-                        if (registeredUserInputAsNumber == 1)
-                        {
-                            wrongLoginOrPassword:
-                            Console.Write("Please eneter your login: ");
-                            string userLogin = Console.ReadLine();
-                            Console.Write("Please eneter your login: ");
-                            string userPassword = Console.ReadLine();
-
-                            if ((userLogin == userNewLogin) && (userPassword == userNewPassword))
-                            {
-                                Console.WriteLine("You have successfuly login");
-                                return;
-                            }
-
-                            else
-                                Console.WriteLine("Incorrect login or password, try again");
-                            goto wrongLoginOrPassword;
-                        }
-                        else
-                            Console.WriteLine("You choose valid number, please choos 1 or press enter to quit");
-                            goto validNumber;
-                    }
+                    Console.WriteLine("Login successful");
                 }
                 else
-                    Console.WriteLine("You choose valid number, please choos 1 or 2, or press enter to quit");
+                {
+                    Console.WriteLine("Login failed, wrong password. Restart Program");
+                }
             }
-            catch (Exception)
+            else
             {
-                Console.WriteLine("General exception, plese choose the correct number");
-                goto generalException;
+                Console.WriteLine("Login failed, wrong username. Restart Program");
             }
-            Console.ReadKey();
         }
+
     }
 }
