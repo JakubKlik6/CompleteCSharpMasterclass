964174c6ffb3096e4d8847ccec3287c0d951b331
Jakub Klik <Jakub.klik6@gmail.com>
2022-10-19 23:54:34 +0200

    Add files via upload

---
+++ Inheritance and more OOP/ElectricalDevice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace completeMasterclass
+{
+    internal class ElectricalDevice
+    {
+        //Parent class
+        public bool IsOn { get; set; }
+        public string Brand { get; set; }
+
+        public ElectricalDevice(bool IsOn, string Brand)
+        {
+            this.IsOn = IsOn;
+            this.Brand = Brand;
+        }
+
+        public void SwitchOn()
+        {
+            IsOn = true;
+        }
+
+        public void SwitchOff()
+        {
+            IsOn = false;
+        }
+    }
+}
---
+++ Inheritance and more OOP/Radio.cs
@@ -0,0 +1,29 @@
+﻿using completeMasterclass;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inheritance
+{
+    internal class Radio : ElectricalDevice
+    {
+        //child class
+        public Radio (bool IsOn, string Brand) : base(IsOn, Brand)
+        {
+
+        }
+
+        public void IsRadioOn()
+        {
+            if (IsOn)
+            {
+                Console.WriteLine("Radio is on");
+            }
+
+            else
+            {
+                Console.WriteLine("Radio is off");
+            }
+        }
+    }
+}
