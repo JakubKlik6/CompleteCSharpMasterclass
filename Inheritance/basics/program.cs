125f869ecc531fe4d49b663c369ab7bec32e559b
Jakub Klik <Jakub.klik6@gmail.com>
2022-10-20 00:01:34 +0200

    Add files via upload

---
+++ Inheritance/basics/program.cs
@@ -0,0 +1,19 @@
+﻿using inheritance;
+using System;
+
+namespace completeMasterclass
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Radio speaker = new Radio(true, "Marshall");
+            speaker.SwitchOff();
+            speaker.IsRadioOn();
+
+            Tv miTV = new Tv(true, "Xiaomi");
+
+            
+        }
+    }
+}
