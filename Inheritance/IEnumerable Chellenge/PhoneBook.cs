3be004b838b2d735f82ea26c103d48672e5ddd32
Jakub Klik <Jakub.klik6@gmail.com>
2022-10-28 12:11:00 +0200

    Add files via upload

---
+++ Inheritance/IEnumerable Chellenge/PhoneBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IEnumerablechallenge
+{
+    internal class PhoneBook
+    {
+        static void Call()
+        {
+            Console.WriteLine("");
+        }
+
+        Contacts = new List<Contact>{
+                new Contact("Andre", "435797087"),
+                new Contact("Lisa", "435677087"),
+                new Contact("Dine", "3457697087"),
+                new Contact("Sofi", "4367697087")
+            };
+}
+}
---
+++ Inheritance/IEnumerable Chellenge/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IEnumerablechallenge
+{
+    internal class Program
+    {
+        static public void Main(string[] args)
+        {
+            PhoneBook MyPhoneBook = new PhoneBook();
+
+            foreach (Contact contact in MyPhoneBook)
+            {
+                contact.Call();
+            }
+        }
+}
