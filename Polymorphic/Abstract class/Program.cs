8ed4b80b57421c52fd22953937741d8ae61ee844
Jakub Klik <Jakub.klik6@gmail.com>
2022-11-02 18:01:52 +0100

    Update Program.cs

--- Polymorphic/Abstract class/Program.cs
+++ Polymorphic/Abstract class/Program.cs
@@ -1,4 +1,4 @@
-﻿using Csharp_Study;
+using Csharp_Study;
 
 Shape[] shapes = { new Sphere(4), new Cube(5) };
 
@@ -6,4 +6,16 @@ foreach (Shape shape in shapes)
 {
     shape.GetInfo();
     Console.WriteLine($"{shape.Name} has volume of {shape.Volume()}");
+
+    Cube iceCube = shape as Cube;
+    if (iceCube == null)
+        Console.WriteLine("This is no a Cube");
+    if (iceCube is Cube)
+        Console.WriteLine("This is a cube");
+
+    object cube1 = new Cube(7);
+    Cube cube2 = (Cube)cube1;
+
+    Console.WriteLine($"\n{cube2.Name} has a Volume of {cube2.Volume()}");
+
 }
