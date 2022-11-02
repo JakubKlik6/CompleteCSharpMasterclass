c7a8b0f1d8d74dc2830abc388f072dd7c2e5e2b7
Jakub Klik <Jakub.klik6@gmail.com>
2022-11-02 18:32:26 +0100

    Add files via upload

---
+++ Polymorphic/Reading from a text file.cs
@@ -0,0 +1,13 @@
+﻿//Example 1 of reading textx
+string text = System.IO.File.ReadAllText(@"C:\Users\DELL\Desktop\blabla.txt");
+
+Console.WriteLine($"Textfile contains following text: \n{text}");
+
+string[] lines = System.IO.File.ReadAllLines(@"C:\Users\DELL\Desktop\blabla.txt");
+
+Console.WriteLine("\nTextfile contains following text:");
+
+foreach (string line in lines)
+{
+    Console.WriteLine("\t" + line);
+}
