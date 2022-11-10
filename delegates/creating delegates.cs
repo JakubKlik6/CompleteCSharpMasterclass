8a4ba13dfdbf8782f6a8ff1d0d5565727956507d
Jakub Klik <Jakub.klik6@gmail.com>
2022-11-10 15:02:32 +0100

    Create creating delegates.cs

---
+++ delegates/creating delegates.cs
@@ -0,0 +1,45 @@
+internal class Program
+    {
+        //defining delegate 
+        public delegate bool FilterDelegate(Person p);
+        static void Main(string[] args)
+        {
+            Person p1 = new Person() { Name = "Marek", Age = 16 };
+            Person p2 = new Person() { Name = "Jakub", Age = 26 };
+            Person p3 = new Person() { Name = "Jasiu", Age = 24 };
+            Person p4 = new Person() { Name = "Ola", Age = 99 };
+
+            List<Person> people = new List<Person>() { p1, p2, p3, p4 };
+
+            DisplayPeople("Kids", people, IsAdult);
+        }
+
+        static public void DisplayPeople(string title, List<Person> people, FilterDelegate filter)
+        {
+            Console.WriteLine(title);
+
+            foreach (Person p in people)
+            {
+                //check if the person passes filter
+                if (filter(p))
+                {
+                    //print name and age
+                    Console.WriteLine($"{p.Name},{p.Age} years old");
+                }
+            }
+        }
+
+        static public bool IsMinor(Person p)
+        {
+            return p.Age < 18;
+        }
+
+        static public bool IsAdult(Person p)
+        {
+            return p.Age >= 18;
+        }
+
+        static public bool IsSenior(Person p)
+        {
+            return p.Age >= 65;
+        }
